e0a9f3fa900f4b19ac3a45b0c46bfaf740f2c95d
weizhang <785414210@qq.com>
2016-01-05 14:57:37 +0800

    test branch

--- app/src/main/java/com/example/kimo/daygo/util/Main.java
+++ app/src/main/java/com/example/kimo/daygo/util/Main.java
@@ -1,14 +1,7 @@
 package com.example.kimo.daygo.util;
 
-import java.io.File;
-
 /**
- * Created by Administrator on 2015/12/23 0023.
+ * Created by Administrator on 2016/1/5 0005.
  */
 public class Main {
-    public static void main(String args[]){
-        String date = Utils.dateFormat(System.currentTimeMillis(),"yy/MM/dd");
-        System.out.println(date);
-//        File path = new File("D:\\UserData\\My Documents\\My Pictures\\粗线性图标\\dayGo");
-    }
 }
